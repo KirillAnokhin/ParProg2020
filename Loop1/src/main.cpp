1544f16ad37cd813b3467ea3eceda0b53a232f9b
Kirill Anokhin <kirya.anokhin.99@mail.ru>
2020-11-06 15:49:12 +0300

    Solution for Loop2

--- Loop1/src/main.cpp
+++ Loop1/src/main.cpp
@@ -5,18 +5,79 @@
 #include <unistd.h>
 #include <cmath>
 
+#define ROOT 0
+
+void calc_sin(double* arr, const uint32_t &xSize, 
+              const uint32_t &beg, const uint32_t &end) {
+    for (uint32_t i = 0; i < (end - beg) * xSize; i++) {
+        arr[i] = sin(0.00001 * arr[i]);
+    }
+}
+
+void master_calc (double *arr, const uint32_t &xSize, const uint32_t &ySize, 
+                  const uint32_t &task_sz, const int &size) {
+  MPI_Status status;
+  
+  uint32_t beg, end;
+  for (int i = 1; i < size; i++) {
+    beg = i * task_sz;
+    end = (i + 1) * task_sz;
+    if (end > ySize) {
+      end = ySize;
+    }
+    if (beg > ySize) {
+      beg = ySize;
+    }
+    MPI_Send(&(arr[beg * xSize]), xSize * (end - beg), MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+  }
+        
+  beg = 0;
+  end = task_sz < ySize ? task_sz : ySize;
+
+  calc_sin(arr, xSize,beg, end);
+  for (int i = 1; i < size; i++) {
+    beg = i * task_sz;
+    end = (i + 1) * task_sz;
+    if (end > ySize)
+      end = ySize;
+    if (beg > ySize)
+      beg = ySize;
+    MPI_Recv (&(arr[beg * xSize]), xSize * (end - beg), MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &status);
+  }
+}
+
+void slave_calc (const uint32_t &xSize, const uint32_t & ySize, 
+                 const uint32_t &rank, const uint32_t & task_sz) {
+  MPI_Status status;
+  double *task_arr;
+  uint32_t beg = rank * task_sz;
+  uint32_t end = (rank + 1) * task_sz;
+  if (end > ySize)
+    end = ySize;
+  if (beg > ySize)
+    beg = ySize;
+ 
+  task_arr = (double*) malloc (sizeof(double) * xSize * (end - beg));
+        
+  MPI_Recv(task_arr, xSize * (end - beg), MPI_DOUBLE, ROOT, 0, MPI_COMM_WORLD, &status);
+  calc_sin(task_arr, xSize, beg, end);
+  MPI_Send(task_arr, xSize * (end - beg), MPI_DOUBLE, ROOT, 0, MPI_COMM_WORLD);
+  free(task_arr);
+}
+
 void calc(double* arr, uint32_t ySize, uint32_t xSize, int rank, int size)
 {
-  if (rank == 0 && size > 0)
-  {
-    for (uint32_t y = 0; y < ySize; y++)
-    {
-      for (uint32_t x = 0; x < xSize; x++)
-      {
-        arr[y*xSize + x] = sin(0.00001*arr[y*xSize + x]);
-      }
+    MPI_Bcast(&ySize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&xSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    uint32_t task_sz = (ySize + size) / size;
+    
+    if (rank == ROOT && size > 0) {
+      master_calc(arr, xSize, ySize, task_sz, size);
+    } 
+    else {
+      slave_calc(xSize, ySize, rank, task_sz);
     }
-  }
 }
 
 int main(int argc, char** argv)
